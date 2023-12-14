0714f87307cb1c5306a06768b3f719eb15d6db38
techwick73 <techwick73@gmail.com>
2023-12-14 17:04:43 +0000

    first commit

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "my_ip_with_cidr" {
+  type = string
+}
+
+variable "public_key" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "server_name" {
+  type = string
+}
+
